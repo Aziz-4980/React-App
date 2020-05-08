0da6c964eb8e9f59f6ef7dcb0c2be2e478becc11
Abdul Aziz Khan <abdul.aziz.khan.1122@gmail.com>
2020-05-08 17:11:03 +0500

    Push to server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -30,7 +30,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
 
-        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
 
     }
 
@@ -124,7 +124,7 @@ if(dish!==null)
     
 
 
-function RenderComments({ comments, addComment, dishId }) {
+function RenderComments({ comments, postComment, dishId }) {
     if (comments != null) {
         const DATE_OPTIONS = { year: 'numeric', month: 'short', day: 'numeric' };
 
@@ -142,7 +142,7 @@ function RenderComments({ comments, addComment, dishId }) {
                 <ul className='list-unstyled'>
                     {cmnts}
                 </ul>
-                <CommentForm dishId={dishId} addComment={addComment} />
+                <CommentForm dishId={dishId} postComment={postComment} />
             </div>
         );
 
@@ -188,7 +188,7 @@ const DishDetail = (props) => {
                 <div className="row">
                     <RenderDish dish={props.dish} />
                     <RenderComments comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         dishId={props.dish.id}
                     />                    </div>
             </div>
